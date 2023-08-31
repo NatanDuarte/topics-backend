7a2a6fc378a54d7cf9a193b10aac85522d83c730
NatanDuarte <natanprocopio@gmail.com>
2023-08-31 09:58:27 -0300

    protecting endpoints with allowed roles

--- api/routes/permissionRoute.js
+++ api/routes/permissionRoute.js
@@ -1,14 +1,15 @@
 const { Router } = require('express');
-
+const roles = require('../middleware/roles');
 const PermissionController = require('../controllers/permissionController');
 const router = Router();
 
+const allowedRoles = ['admin', 'dev']
 
 router
-    .post('/permission', PermissionController.register)
-    .get('/permission', PermissionController.getAll)
-    .get('/permission/:id', PermissionController.getById)
-    .put('/permission/:id', PermissionController.update)
-    .delete('/permission/:id', PermissionController.delete);
+    .post('/permission', roles(allowedRoles), PermissionController.register)
+    .get('/permission', roles(allowedRoles), PermissionController.getAll)
+    .get('/permission/:id', roles(allowedRoles), PermissionController.getById)
+    .put('/permission/:id', roles(allowedRoles), PermissionController.update)
+    .delete('/permission/:id', roles(allowedRoles), PermissionController.delete);
 
 module.exports = router;
